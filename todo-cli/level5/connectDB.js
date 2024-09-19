ba9713a1d65d91ce538e121f7a10bd5ed7f3929e
Velavan5 <gvelavan2005@gmail.com>
2024-09-19 21:36:45 +0530

    setup is ready , see you later

---
+++ todo-cli/level5/connectDB.js
@@ -0,0 +1,23 @@
+
+
+
+//To use postgres , You have to start it ,by using :- systemctl start postgresql     !!don't forget
+const Sequelize = require("sequelize");
+
+const database = "todo_db";
+const username = "postgres";
+const password = "post";
+const sequelize = new Sequelize(database, username, password, {
+  host: "localhost",
+  dialect: "postgres",
+  // logging: false
+});
+
+const connect = async () => {
+  return sequelize.authenticate();
+}
+
+module.exports = {
+  connect,
+  sequelize
+}
